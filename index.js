6fae0fdead9371f5550a60c896c27d70d5f51268
Kylee Tilley <kyleetilley@gmail.com>
2018-10-23 20:09:36 -0500

    Reformat

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const assert = require("assert");
 
-/**
- *
- * @param {*} actual
- * @param  {...any} matchers
- */
 const assertThat = (actual, ...matchers) => {
   const errors = matchers.reduce(runMatcher(actual), []);
   if (errors.length > 0) throw new Error(errors);
@@ -24,32 +19,42 @@ const assertThat = (actual, ...matchers) => {
 const equals = expected => actual =>
   assert(actual == expected, `expected ${actual} to equal ${expected}`);
 
-const strictEquals = expected => actual =>
+equals.strict = expected => actual =>
   assert(actual === expected, `expected ${actual} to strict equal ${expected}`);
 
-const is = strictEquals;
+const is = equals.strict;
 
 is.true = actual => assert(actual === true, `expected ${actual} to be true`);
-
-is.truthy = actual => assert(actual, `expected ${actual} to be truthy`);
-
 is.false = actual => assert(actual === false, `expected ${actual} to be false`);
-
+is.truthy = actual => assert(actual, `expected ${actual} to be truthy`);
 is.falsey = actual => assert(!actual, `expected ${actual} to be falsey`);
 
 is.empty = actual =>
   assert(actual.length === 0, `expected ${actual} to be empty`);
 
-const includes = expected => actual =>
+const has = {};
+
+has.item = expected => actual =>
   assert(
     actual.includes(expected),
     `expected that ${actual} includes ${expected}`
   );
 
-const hasLength = expected => actual =>
+has.items = (...expected) => actual =>
+  assert(
+    expected.every(item => item.includes(actual)),
+    `expected that ${actual} includes ${expected}`
+  );
+
+has.length = expected => actual =>
   assert(
     actual.length === expected,
     `expected that ${actual} has length of ${expected}`
   );
 
-assertThat("", is.empty);
+module.exports = {
+  assertThat,
+  equals,
+  is,
+  has
+};
